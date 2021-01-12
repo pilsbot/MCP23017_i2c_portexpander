f1c8565382c16cc9f19981576d13b4b551b8983a
Pascal Pieper <Accounts@PascalPieper.de>
2021-01-12 17:28:28 +0100

    rclcpp seems to miss the correct inference for bind

--- src/portexpander-bridge.cpp
+++ src/portexpander-bridge.cpp
@@ -20,10 +20,9 @@ class Portexpander_I2C_Bridge : public rclcpp::Node
 		{
 			for(uint_fast8_t bit = 0; bit < 8; bit++)
 			{
-				std::string topic_name = base_name + (port == 0 ? "A" : "B");
-				topic_name += "/_" + std::to_string(bit);
+				std::string topic_name = base_name + (port == 0 ? "A" : "B") + std::to_string(bit);
 				std::cout << "Registering topic " << topic_name << std::endl;
-				auto kack = std::bind(&Portexpander_I2C_Bridge::topic_callback, this, _1);//, port, bit);
+				std::function<void(const std_msgs::msg::Bool::SharedPtr)> kack = std::bind(&Portexpander_I2C_Bridge::topic_callback, this, _1, port, bit);
 				_subscriptions.emplace_back(
 										this->create_subscription<std_msgs::msg::Bool>(
 											topic_name, 1, kack)
@@ -36,11 +35,9 @@ class Portexpander_I2C_Bridge : public rclcpp::Node
     }
 
   private:
-    void topic_callback(const std_msgs::msg::Bool::SharedPtr msg)
-						//const uint_fast8_t port, const uint_fast8_t bitpos)
+    void topic_callback(const std_msgs::msg::Bool::SharedPtr msg,
+						const uint_fast8_t port, const uint_fast8_t bitpos)
     {
-		const uint_fast8_t port = 0;
-		const uint_fast8_t bitpos = 0;
 		assert(port < 2 && "Only Port A or B valid");
 		RCLCPP_INFO(this->get_logger(), "Set port %s bit %u to %s",
 				 port == 0 ? "A" : "B", bitpos, msg->data ? "1" : "0");
